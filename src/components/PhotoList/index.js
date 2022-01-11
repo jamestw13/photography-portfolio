7c1580e4b8596b54ca906f20f2697f558b8156ee
TJ James <jamestw13@gmail.com>
2022-01-11 14:18:06 -0600

    created modal and added functionality from photolist

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Modal from '../../components/Modal';
 
 function PhotoList({category}) {
   const [photos] = useState([
@@ -99,14 +100,24 @@ function PhotoList({category}) {
     },
   ]);
 
+  const [currentPhoto, setCurrentPhoto] = useState();
+  function toggleModal(image, i) {
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(true);
+  }
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const currentPhotos = photos.filter(photo => photo.category === category);
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       {currentPhotos.map((image, i) => (
         <img
           src={require(`../../assets/small/${category}/${i}.jpg`)}
           alt={image.name}
           className="img-thumbnail mx-1"
+          onClick={() => toggleModal(image, i)}
           key={image.name}
         />
       ))}
